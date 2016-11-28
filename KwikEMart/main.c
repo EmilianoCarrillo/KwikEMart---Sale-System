855e3d8a95c98dc41d8554d3f2e3f2131e05c26c
Emiliano Carrillo <emilianocarrillo@imac-de-emiliano.local>
2016-11-28 15:52:34 -0600

    Initial Commit

---
+++ KwikEMart/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  KwikEMart
+//
+//  Created by Emiliano Carrillo on 28/11/16.
+//  Copyright © 2016 Emiliano Carrillo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
